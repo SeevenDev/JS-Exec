e7b3adb53ba9dc17c8a8989f2f16745b0630e6ba
Seeven <seeven.dev@gmail.com>
2014-09-25 20:26:31 +0200

    Added style to the options page

--- js_exec/js/options.js
+++ js_exec/js/options.js
@@ -1,9 +1,9 @@
-$(document).ready(function() 
+$(document).ready(function()
 {
 	// ======================================================================
-	// === Remplissage de la sidebar
+	// === REMPLISSAGE DE #listScripts
 	// ======================================================================
-	
+
 	if (!!localStorage['user_scripts']) {
 		$.each(localStorage['user_scripts'], function(name, code) {
 			$('#sidebar > ul#scripts').append('<li><a id="link-'+name+'">'+name+'</a></li>');
@@ -13,7 +13,7 @@ $(document).ready(function()
 	// ======================================================================
 	// === Enregistrement d'un script
 	// ======================================================================
-	
+
 	$('#addScript #saveScript').click(function() {
 		var user_scripts = !!localStorage['user_scripts'] ? localStorage['user_scripts']
 		var nom = $('#addScript input[name=nom-script]').val();
