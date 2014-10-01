d6942ccbec19f0421c29ca24af6e8563d76a582b
Seeven <seeven.dev@gmail.com>
2014-10-01 18:43:30 +0200

    Moved executeScript & inject funcs to background.js

--- js_exec/js/background.js
+++ js_exec/js/background.js
@@ -1,1 +1,34 @@
-// console.log('hello');
+
+function inject(script, tabID) {
+	// Inject jQuery :
+	chrome.tabs.executeScript(tabID, {file: 'js/jquery-1.11.1.min.js'});
+	// Inject the script after 0.5 sec :
+	setTimeout(function() {
+		chrome.tabs.executeScript(tabID, {code: script});
+	}, 500);
+}
+
+function executeScript(code_) {
+	// === On active tab ===
+	chrome.tabs.query({active: true, lastFocusedWindow: true}, function(selectedTab) {
+		// --- Splits the code at the ":load:" keywords ---
+		var blocks = code_.split('//:load:\n');
+		console.log(blocks);
+
+		// --- For the first block of code (no page change expected) ---
+		inject(blocks[0], selectedTab[0].id);
+
+		// --- For each page load ---
+		var block_index = 1;
+		chrome.tabs.onUpdated.addListener(function(tabID, changeInfo, tab) {
+			if (changeInfo.status === 'complete' // load is complete
+				&& tabID == selectedTab[0].id) // it's the same page the script was started from
+			{
+				// Inject the next block of code :
+				inject(blocks[block_index], selectedTab[0].id);
+				block_index++;
+			}
+		});
+
+	});
+}
--- js_exec/js/popup.js
+++ js_exec/js/popup.js
@@ -16,40 +16,6 @@ $(document).ready(function()
 		});
 	}
 
-	function inject(script, tabID) {
-		// Inject jQuery :
-		chrome.tabs.executeScript(tabID, {file: 'js/jquery-1.11.1.min.js'});
-		// Inject the script after 0.5 sec :
-		setTimeout(function() {
-			chrome.tabs.executeScript(tabID, {code: script});
-		}, 500);
-	}
-
-	function executeScript(code_) {
-		// === On active tab ===
-		chrome.tabs.query({active: true, lastFocusedWindow: true}, function(selectedTab) {
-			// --- Splits the code at the ":load:" keywords ---
-			var blocks = code_.split('//:load:\n');
-			console.log(blocks);
-
-			// --- For the first block of code (no page change expected) ---
-			inject(blocks[0], selectedTab[0].id);
-
-			// --- For each page load ---
-			var block_index = 1;
-			chrome.tabs.onUpdated.addListener(function(tabID, changeInfo, tab) {
-				if (changeInfo.status === 'complete' // load is complete
-					&& tabID == selectedTab[0].id) // it's the same page the script was started from
-				{
-					// Inject the next block of code :
-					inject(blocks[block_index], selectedTab[0].id);
-					block_index++;
-				}
-			});
-
-		});
-	}
-
 	// ======================================================================
 	// === "CHOISIR UN SCRIPT"
 	// ======================================================================
@@ -66,7 +32,9 @@ $(document).ready(function()
 		chrome.storage.local.get('user_scripts', function(user_scripts) {
 			var scripts_array = user_scripts.user_scripts;
 			var code = scripts_array[nom];
-			executeScript(code);
+			chrome.runtime.getBackgroundPage(function(wind) {
+				wind.executeScript(code);
+			});
 		});
 	});
 
@@ -78,7 +46,9 @@ $(document).ready(function()
 	$('form#quick-script').submit(function(e) {
 		e.preventDefault();
 		var code = $('form#quick-script textarea[name=quick-script]').val();
-		executeScript(code);
+		chrome.runtime.getBackgroundPage(function(wind) {
+			wind.executeScript(code);
+		});
 	});
 
 });
